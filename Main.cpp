08be0b1c842fe126a27e75749bd690496606c30b
xx1level1xx <53699465+xx1level1xx@users.noreply.github.com>
2022-08-07 09:32:23 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -66,6 +66,6 @@ int** getParanthesesStartEndPosLevel(string expression, int& num){
 			}
 		}
 	}
-	//consecutively same level //search for the end of parantheses pair, slow cauz I had studied many important things like Biology and still remember without much factorization
+	//consecutively same level //search for the end of parantheses pair, slow cauz I had studied many important things like Biology and still remember without much factorization and the corresponding level is the same or not
 	return paranthesesStartEndPos;
 }
